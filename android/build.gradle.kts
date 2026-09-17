532c6d0e11fa0373eaffc210f3124619e916ffcb
MatchaChocolate73 <giedeonshabrina@gmail.com>
2026-09-17 19:39:21 +0700

    Fix Android release build

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,14 +9,38 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+// Fix compileSdk untuk plugin Android yang masih menggunakan SDK lama
+subprojects {
+    plugins.withId("com.android.library") {
+        extensions.configure<com.android.build.gradle.LibraryExtension> {
+            compileSdk = 36
+        }
+    }
+}
+
+// Fix JVM target mismatch pada another_telephony
+subprojects {
+    if (name == "another_telephony") {
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            compilerOptions {
+                jvmTarget.set(
+                    org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+                )
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
